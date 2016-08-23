82dab48160ae2c3ece757ae9e4aeabafe115f1fd
Thuy Pham <tpham045@hotmail.com>
2016-08-23 03:46:48 +0000

    Add bootstrap and user show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :users, only: [:show]
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
