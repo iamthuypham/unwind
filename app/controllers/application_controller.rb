54033fa1917faf97589ff4564d50778045e0b100
Thuy Pham <tpham045@hotmail.com>
2016-08-31 02:05:46 +0000

    Add roles. User able to view their current roles in each event

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  ROLES = {0 => 'guest', 1 => 'user', 2 => 'helper', 3 => 'planner'}
+  
+  attr_reader :role
+    
+  def current_role
+    if current_user && @event.user == current_user
+      @role = ROLES[3]
+    elsif current_user && @event.user != current_user
+      @role = ROLES[2]
+    elsif current_user
+      @role = ROLES[1]
+    else
+      @role = ROLES[0]
+    end
+    p @role
+  end
+  helper_method :current_role
 end
