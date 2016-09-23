5b23531c7ed5874c23a7f2e5ecab2897b29e60d4
Thuy Pham <tpham045@hotmail.com>
2016-09-23 03:31:41 +0000

    Add Date field to Activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,8 @@
 class Activity < ActiveRecord::Base
   belongs_to :event
+  after_save :date?
+  
+  def self.date?(activity)
+    activity.event.length == 'One Day' ? activity.event.startdate : activity.date
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923003513) do
+ActiveRecord::Schema.define(version: 20160923012052) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160923003513) do
     t.integer  "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "date"
   end
 
   add_index "activities", ["event_id"], name: "index_activities_on_event_id"
