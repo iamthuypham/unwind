b9f44886859cadff73a14262a371de3f2dc267b8
Thuy Pham <tpham045@hotmail.com>
2016-09-25 21:06:39 +0000

    Reorganize with canvas and off-canvas menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,29 @@
 //= require turbolinks
  //= require bootstrap
 //= require_tree .
+/*global $*/
+/*====================================
+=            ON DOM READY            =
+====================================*/
+$(function() {
+  $('.toggle-nav').click(function() {
+    // Calling a function in case you want to expand upon this.
+    toggleNav();
+  });
+});
+
+
+/*========================================
+=            CUSTOM FUNCTIONS            =
+========================================*/
+function toggleNav() {
+if ($('#site-wrapper').hasClass('show-nav')) {
+  // Do things on Nav Close
+  $('#site-wrapper').removeClass('show-nav');
+} else {
+  // Do things on Nav Open
+  $('#site-wrapper').addClass('show-nav');
+}
+
+//$('#site-wrapper').toggleClass('show-nav');
+}
